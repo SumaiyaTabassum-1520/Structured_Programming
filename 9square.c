1581a564e3f088f1a31e15b95e8bdc08d09b0248
SumaiyaTabassum-1520 <105984307+SumaiyaTabassum-1520@users.noreply.github.com>
2022-10-09 13:12:14 +0600

    Whether the number is a perfect square or not

---
+++ 9square.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,n,t,j;
+    scanf("%d",&n);
+    for(i=1 ; i<=n ; i++)
+    {
+        scanf("%d",&t);
+        for(j=1 ; j<=n ; j++)
+        {
+
+        }
+         if(sqrt(t)*sqrt(t)==t)
+            {
+                printf("yes\n");
+            }
+            else
+                printf("no\n");
+    }
+
+    return 0;
+}
